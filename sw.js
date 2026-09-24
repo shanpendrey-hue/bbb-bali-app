7c8968fabebff287e6e5b4327e4b5b89525468c8
shannonpendrey <shannon@cs-8.com.au>
2026-09-24 20:42:52 +0800

    Version 6.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bbb-bali-v5-master-5';
+const CACHE='bbb-bali-v5-made-flights-1';
 const SHELL=['/','/index.html','/styles.css','/app.js','/manifest.json','/icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
