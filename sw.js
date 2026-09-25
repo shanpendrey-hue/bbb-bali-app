8e475bb7227c863bf69f15a5a64fa2dba4483c03
shannonpendrey <shannon@cs-8.com.au>
2026-09-25 09:00:52 +0800

    Version 7.2.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bbb-bali-bash-board-v3-hotfix1';
+const CACHE='bbb-bali-bash-v3-2-menu-hotfix';
 const SHELL=['/','/index.html','/styles.css','/app.js','/manifest.json','/icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
