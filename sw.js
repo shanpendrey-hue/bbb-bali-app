c0d425c36ffc15073055b02b859ec0e1c421c67b
shannonpendrey <shannon@cs-8.com.au>
2026-09-26 06:30:35 +0800

    Version 24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bbb-cache-v32-photo-galleries';
+const CACHE='bbb-cache-v33-chat-swipe-fix';
 const SHELL=['/','/index.html','/styles.css','/app.js','/manifest.json','/assets/bbb-logo.png','/assets/icons/icon-192.png','/assets/icons/icon-512.png','/assets/icons/icon-maskable-512.png','/assets/icons/apple-touch-icon.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
